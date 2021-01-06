51ef1fa29020d08de98697baddf625a881b128ed
appypatel <akpatelak5525@gmail.com>
2021-01-05 19:32:15 -0500

    Set up eureka naming server

---
+++ src/test/java/com/microservices/netfiixeurekanamingserver/NetfiixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.netfiixeurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetfiixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
